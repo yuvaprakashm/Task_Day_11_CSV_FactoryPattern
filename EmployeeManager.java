7d106eabe30e0810543ffb65aa52746813d827ca
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-19 15:49:03 +0530

    Update EmployeeManager.java

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -1,14 +1,14 @@
 package net.texala.employee.manager;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.InputMismatchException;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Vector;
 
 import net.texala.employee.constants.Constants;
@@ -23,25 +23,24 @@ public class EmployeeManager {
 		loadDataFromCSV();
 	}
 
-	private void loadDataFromCSV() {
-		try (BufferedReader reader = new BufferedReader(new FileReader(Constants.FILE_NAME))) {
-			String line;
-			boolean headerSkipped = false;
-			while ((line = reader.readLine()) != null) {
-				if (!headerSkipped) {
-					headerSkipped = true;
-					continue;
-				}
-				String[] parts = line.split(Constants.CSV_DELIMITER);
-				if (parts.length == Constants.NUMBER_OF_FIELDS) {
-					int empId = Integer.parseInt(parts[Constants.EMPLOYEE_ID_INDEX]);
-					String firstName = parts[Constants.FIRST_NAME_INDEX];
-					String lastName = parts[Constants.LAST_NAME_INDEX];
-					String department = parts[Constants.DEPARTMENT_INDEX];
-					employee.add(new Employee(empId, firstName, lastName, department));
-				}
+	public void loadDataFromCSV() {
+		try {
+			File file = new File(Constants.FILE_NAME);
+			if (!file.exists()) {
+
+				file.createNewFile();
+				System.out.println(Constants.NEW_CSV);
+				return;
 			}
-		} catch (IOException | NumberFormatException e) {
+
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+
+				System.out.println(line);
+			}
+			scanner.close();
+		} catch (IOException e) {
 			System.out.println(Constants.LOAD_CSV_ERROR + e.getMessage());
 		}
 	}
@@ -49,7 +48,7 @@ public class EmployeeManager {
 	public void addEmployee(int empId, String firstName, String lastName, String department) {
 		if (!isValidName(firstName) && !isValidName(lastName)) {
 			System.out.println(Constants.INVALID_NAME_ERROR);
-			logError(Constants.INVALID_NAME_LOG + firstName + lastName);
+			logError(Constants.INVALID_NAME_LOG + firstName + ',' + lastName);
 			return;
 		}
 		if (!isValidName(firstName)) {
@@ -94,6 +93,21 @@ public class EmployeeManager {
 		}
 	}
 
+	public void updateEmployee(int empIdToUpdate, String fieldToUpdate, int newEmpId, String newFirstName,
+			String newLastName, String newDepartment) {
+		for (Employee employee : employee) {
+			if (employee.getEmpId() == empIdToUpdate) {
+				employee.setEmpId(newEmpId);
+				employee.setFirstName(newFirstName);
+				employee.setLastName(newLastName);
+				employee.setDepartment(newDepartment);
+				System.out.println(Constants.UPDATE_SUCCESS_MESSAGE);
+				return;
+			}
+		}
+		System.out.println(Constants.EMPLOYEE_NOT_FOUND_ERROR);
+	}
+
 	public void updateEmployee(int empIdToUpdate, String fieldToUpdate, String newValue) {
 		boolean employeeFound = false;
 		for (Employee employee : employee) {
@@ -120,21 +134,6 @@ public class EmployeeManager {
 		if (!employeeFound) {
 			throw new IllegalArgumentException(Constants.EMPLOYEE_NOT_FOUND_ERROR + empIdToUpdate);
 		}
-	}
-
-	public void updateEmployee(int empIdToUpdate, String fieldToUpdate, int newEmpId, String newFirstName,
-			String newLastName, String newDepartment) {
-		for (Employee employee : employee) {
-			if (employee.getEmpId() == empIdToUpdate) {
-				employee.setEmpId(newEmpId);
-				employee.setFirstName(newFirstName);
-				employee.setLastName(newLastName);
-				employee.setDepartment(newDepartment);
-				System.out.println(Constants.UPDATE_SUCCESS_MESSAGE);
-				return;
-			}
-		}
-		System.out.println(Constants.EMPLOYEE_NOT_FOUND_ERROR);
 	}
 
 	public void deleteEmployees(int empIdToDelete) {
@@ -176,11 +175,23 @@ public class EmployeeManager {
 		}
 	}
 
-	public Vector<Employee> getAllEmployees() {
+	public Vector<Employee> displayAllEmployees() {
+		System.out.println(Constants.EMPLOYEE_HEADER);
 		return employee;
 	}
 
+	public Employee getEmployeeByEmpId(int empId) {
+
+		for (Employee emp : employee) {
+			if (emp.getEmpId() == empId) {
+				return emp;
+			}
+		}
+		return null;
+	}
+
 	public Vector<Employee> getEmployeesByEmpId(int empId) {
+		 
 		Vector<Employee> employeesWithSameId = new Vector<>();
 		for (Employee emp : employee) {
 			if (emp.getEmpId() == empId) {
@@ -196,23 +207,54 @@ public class EmployeeManager {
 	}
 
 	public static void displaySortedEmployeeRecords(EmployeeManager manager, Comparator<Employee> comparator) {
-		Vector<Employee> sortedEmployees = new Vector<>(manager.getAllEmployees());
+		Vector<Employee> sortedEmployees = new Vector<>(manager.displayAllEmployees());
 		Collections.sort(sortedEmployees, comparator);
 		if (sortedEmployees.isEmpty()) {
 			System.out.println(Constants.NO_EMPLOYEES_FOUND_MESSAGE);
 		} else {
-			System.out.println(Constants.SORTED_EMPLOYEE_RECORDS_HEADER);
+
+			 
 			for (Employee emp : sortedEmployees) {
 				System.out.println(emp);
 			}
+
 			storeSortedRecordsInCSV(sortedEmployees);
 		}
+	}
+
+	public void displaySortedRecordsByOption(EmployeeManager manager, Scanner scanner) {
+		System.out.println(Constants.SORT_BY);
+		System.out.println(Constants.SORT_OPTION_EMPID);
+		System.out.println(Constants.SORT_OPTION_FIRST_NAME);
+		System.out.println(Constants.SORT_OPTION_LAST_NAME);
+		System.out.print(Constants.ENTER_CHOICE);
+		int sortOption = scanner.nextInt();
+		scanner.nextLine();
+
+		Comparator<Employee> comparator;
+
+		switch (sortOption) {
+		case 1:
+			comparator = Comparator.comparingInt(Employee::getEmpId);
+			break;
+		case 2:
+			comparator = Comparator.comparing(Employee::getFirstName);
+			break;
+		case 3:
+			comparator = Comparator.comparing(Employee::getLastName);
+			break;
+		default:
+			System.out.println(Constants.INVALID_OPTION_SELECTED);
+			return;
+		}
+
+		EmployeeManager.displaySortedEmployeeRecords(manager, comparator);
 	}
 
 	private static void storeSortedRecordsInCSV(Vector<Employee> employees) {
 		String sortedFileName = Constants.SORTED_FILE;
 		try (FileWriter fileWriter = new FileWriter(sortedFileName)) {
-			fileWriter.write(Constants.CSV_HEADER);
+		 fileWriter.write(Constants.CSV_HEADER);
 			for (Employee emp : employees) {
 				fileWriter.write(emp.getEmpId() + Constants.CSV_DELIMITER + emp.getFirstName() + Constants.CSV_DELIMITER
 						+ emp.getLastName() + Constants.CSV_DELIMITER + emp.getDepartment() + Constants.NEW_LINE);
@@ -234,4 +276,84 @@ public class EmployeeManager {
 			}
 		}
 	}
+
+	public int getValidEmpId(Scanner scanner) {
+		while (true) {
+			try {
+				int empId = Integer.parseInt(scanner.nextLine().trim());
+				if (empId < 0) {
+					System.out.println(Constants.ERROR_INVALID_EMPID);
+					System.out.print(Constants.ENTER_EMPID + ": ");
+					continue;
+				}
+				return empId;
+			} catch (NumberFormatException e) {
+				System.out.println(Constants.ERROR_INVALID_EMPID);
+				System.out.print(Constants.ENTER_EMPID + ": ");
+			}
+		}
+	}
+
+	public String selectDepartment(Scanner scanner) {
+		String department = Constants.UNKNOWN_DEPARTMENT;
+		boolean validChoice = false;
+
+		while (!validChoice) {
+			try {
+				System.out.println(Constants.ENTER_DEPARTMENT);
+				System.out.println(Constants.DEPARTMENT_OPTIONS);
+				System.out.print(Constants.SELECT_DEPARTMENT_PROMPT);
+
+				int departmentChoice = scanner.nextInt();
+				scanner.nextLine();
+
+				switch (departmentChoice) {
+				case 1:
+					department = Constants.DEV_DEPARTMENT;
+					validChoice = true;
+					break;
+				case 2:
+					department = Constants.PS_DEPARTMENT;
+					validChoice = true;
+					break;
+				case 3:
+					department = Constants.QA_DEPARTMENT;
+					validChoice = true;
+					break;
+				case 4:
+					department = Constants.ADMIN_DEPARTMENT;
+					validChoice = true;
+					break;
+				default:
+					System.out.println(Constants.INVALID_DEPARTMENT_DEFAULTING);
+				}
+			} catch (InputMismatchException e) {
+				scanner.nextLine();
+				System.out.println(Constants.ERROR_INVALID_INPUT);
+			}
+		}
+
+		return department;
+	}
+
+	public int getDeletionChoice(Scanner scanner) {
+		int choice = 0;
+		boolean validInput = false;
+		while (!validInput) {
+			try {
+				System.out.println(Constants.DELETE_EMPLOYEE_OPTIONS);
+				System.out.print(Constants.ENTER_CHOICE);
+				choice = scanner.nextInt();
+				scanner.nextLine();
+				validInput = true;
+			} catch (InputMismatchException e) {
+
+				scanner.nextLine();
+				System.out.println(Constants.INVALID_CHOICE);
+			}
+		}
+		return choice;
+	}
+
+	 
 }
