ff9632ec857219d1163fe8bf4977b66a428c765e
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-19 15:51:23 +0530

    Update displayEmployee.java

--- displayEmployee.java
+++ displayEmployee.java
@@ -1,6 +1,5 @@
 package net.texala.employee.impl;
 
-import java.util.Comparator;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -11,79 +10,53 @@ import net.texala.employee.model.Employee;
 import net.texala.employee.service.EmployeeOperation;
 
 public class displayEmployee implements EmployeeOperation {
-    @Override
+	@Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
-    	try {
-    		System.out.println(Constants.DISPLAY_OPTION_ALL);
-            System.out.println(Constants.DISPLAY_OPTION_EMPID);
-            System.out.println(Constants.DISPLAY_OPTION_SORTED);
-            System.out.print(Constants.ENTER_CHOICE);
-		int displayOption = scanner.nextInt();
-		scanner.nextLine();
-		switch (displayOption) {
-		case 1:
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                System.out.println(Constants.DISPLAY_OPTION_ALL);
+                System.out.println(Constants.DISPLAY_OPTION_EMPID);
+                System.out.println(Constants.DISPLAY_OPTION_SORTED);
+                System.out.print(Constants.ENTER_CHOICE);
+                int displayOption = scanner.nextInt();
+                scanner.nextLine();  
 
-			Vector<Employee> allEmployees = manager.getAllEmployees();
-			if (allEmployees.isEmpty()) {
-				System.out.println(Constants.NO_EMPLOYEES_FOUND);
-			} else {
-				System.out.println(Constants.EMPLOYEE_HEADER);
-				for (Employee emp : allEmployees) {
-					System.out.println(emp);
-				}
-			}
-			break;
-		case 2:
+                switch (displayOption) {
+                    case 1:
+                    	manager.displayAllEmployees().forEach(System.out::println);
+                        validInput = true;
+                        break;
+                    case 2:
+                        System.out.println(Constants.ENTER_EMPID);
+                        int empId = scanner.nextInt();
+                        scanner.nextLine();   
+                        Vector<Employee> employees = manager.getEmployeesByEmpId(empId);
+                        if (employees.isEmpty()) {
+                            System.out.println(Constants.EMP_NOTFOUND);
+                        } else {
+                            System.out.println(Constants.EMPID + empId + Constants.SEMI);
+                            for (Employee emp : employees) {
+                                System.out.println(emp); 
+                            }
+                        }
+                        validInput = true;
+                        break;
 
-			System.out.println(Constants.ENTER_EMPID);
-			int empIdToDisplay = scanner.nextInt();
-			scanner.nextLine();
-			Vector<Employee> employeesWithSameId = manager.getEmployeesByEmpId(empIdToDisplay);
-			if (employeesWithSameId.isEmpty()) {
-				System.out.println(Constants.EMPLOYEE_NOT_FOUND);
-			} else {
-				System.out.println(Constants.ENTER_EMPID);
-				for (Employee emp : employeesWithSameId) {
-					System.out.println(emp);
-				}
-			}
-			break;
-		case 3:
+                        
+                    case 3:
+                    	manager.displaySortedRecordsByOption(manager, scanner);
+                        validInput = true;
+                        break;
 
-			System.out.println(Constants.SORT_BY);
-			System.out.println(Constants.SORT_OPTION_EMPID);
-			System.out.println(Constants.SORT_OPTION_FIRST_NAME);
-			System.out.println(Constants.SORT_OPTION_LAST_NAME);
-			System.out.print(Constants.ENTER_CHOICE);
-			int sortOption = scanner.nextInt();
-			scanner.nextLine();
-			switch (sortOption) {
-			case 1:
-
-				EmployeeManager.displaySortedEmployeeRecords(manager,
-						Comparator.comparingInt(Employee::getEmpId));
-				break;
-			case 2:
-
-				EmployeeManager.displaySortedEmployeeRecords(manager,
-						Comparator.comparing(Employee::getFirstName));
-				break;
-			case 3:
-
-				EmployeeManager.displaySortedEmployeeRecords(manager,
-						Comparator.comparing(Employee::getLastName));
-				break;
-			default:
-				System.out.println(Constants.INVALID_OPTION_SELECTED);
-				break;
-			}
-			break;  
-		default:
-			System.out.println(Constants.INVALID_OPTION_SELECTED);
-			break;
-		}
-    }catch (Exception e) {
-      	 EmployeeOperationException.throwDisplayEmployeeException(Constants.ERROR_DISPLAY_EMPLOYEE + e.getMessage());
+                    default:
+                        System.out.println(Constants.INVALID_OPTION_SELECTED);
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(Constants.ERROR_INVALID_INPUT);
+                scanner.nextLine();  
+            }
+        }
     }
-  }
-  }
+}
