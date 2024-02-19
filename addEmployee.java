a53f494bbe28511f0170b14535ef5e3c03a355d5
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-19 15:50:35 +0530

    Update addEmployee.java

--- addEmployee.java
+++ addEmployee.java
@@ -1,3 +1,4 @@
+
 package net.texala.employee.impl;
 
 import java.util.Scanner;
@@ -8,49 +9,24 @@ import net.texala.employee.service.EmployeeOperation;
 import net.texala.employee.exceptions.EmployeeOperationException;
 
 public class addEmployee implements EmployeeOperation {
-    @Override
-    public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
-        try {
-            System.out.println(Constants.ENTER_EMPID);
-            int empId = scanner.nextInt();
-            scanner.nextLine();
+	@Override
+	public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
+		try {
+			System.out.println(Constants.ENTER_EMPID);
+			int empId = manager.getValidEmpId(scanner);
 
-            System.out.println(Constants.ENTER_FIRST_NAME);
-            String firstName = scanner.nextLine();
+			System.out.println(Constants.ENTER_FIRST_NAME);
+			String firstName = scanner.nextLine();
 
-            System.out.println(Constants.ENTER_LAST_NAME);
-            String lastName = scanner.nextLine();
+			System.out.println(Constants.ENTER_LAST_NAME);
+			String lastName = scanner.nextLine();
 
-            System.out.println(Constants.ENTER_DEPARTMENT);
-            System.out.println(Constants.DEPARTMENT_OPTIONS);
-            System.out.print(Constants.SELECT_DEPARTMENT_PROMPT);
-            int departmentChoice = scanner.nextInt();
-            scanner.nextLine();
+			String department = manager.selectDepartment(scanner);
 
-            String department;
-            switch (departmentChoice) {
-                case 1:
-                    department = Constants.DEV_DEPARTMENT;
-                    break;
-                case 2:
-                    department = Constants.PS_DEPARTMENT;
-                    break;
-                case 3:
-                    department = Constants.QA_DEPARTMENT;
-                    break;
-                case 4:
-                    department = Constants.ADMIN_DEPARTMENT;
-                    break;
-                default:
-                    System.out.println(Constants.INVALID_DEPARTMENT_DEFAULTING);
-                    department = Constants.UNKNOWN_DEPARTMENT;
-                    break;
-            }
+			manager.addEmployee(empId, firstName, lastName, department);
+		} catch (Exception e) {
 
-            manager.addEmployee(empId, firstName, lastName, department);
-        } catch (Exception e) {
-        	
-            EmployeeOperationException.throwAddEmployeeException(Constants.ERROR_ADD_EMPLOYEE + e.getMessage());
-        }
-    }
+			EmployeeOperationException.throwAddEmployeeException(Constants.ERROR_ADD_EMPLOYEE + e.getMessage());
+		}
+	}
 }
