6c73cb80c019164aec00a482dc4f33d0a3853f00
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-19 15:51:08 +0530

    Update deleteEmployee.java

--- deleteEmployee.java
+++ deleteEmployee.java
@@ -1,6 +1,5 @@
 package net.texala.employee.impl;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 import net.texala.employee.manager.EmployeeManager;
 import net.texala.employee.service.EmployeeOperation;
@@ -11,33 +10,21 @@ public class deleteEmployee implements EmployeeOperation {
     @Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
         try {
-            System.out.println(Constants.MENU_OPTION_DELETE_SPECIFIC);
-            System.out.println(Constants.MENU_OPTION_DELETE_ALL);
-            System.out.print(Constants.MENU_OPTION_ENTER_CHOICE);
-            int deleteOption = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (deleteOption) {
-                case 1:
-                    System.out.println(Constants.DELETE_EMPLOYEE_PROMPT);
-                    int empIdToDelete = scanner.nextInt();
-                    scanner.nextLine();
-                    boolean isDeleted = manager.deleteEmployeeById(empIdToDelete);  
-                    if (isDeleted) {
-                        System.out.println(String.format(Constants.SUCCESS_EMPLOYEE_DELETED, empIdToDelete));
-                    } else {
-                        throw new EmployeeOperationException(String.format(Constants.ERROR_EMPLOYEE_NOT_FOUND, empIdToDelete));
-                    }
-                    break;
-                case 2:
-                    manager.deleteAllEmployees();
-                    System.out.println(Constants.SUCCESS_ALL_EMPLOYEES_DELETED);
-                    break;
-                default:
-                    throw new EmployeeOperationException(Constants.ERROR_INVALID_OPTION);
+            int choice = manager.getDeletionChoice(scanner);
+            if (choice == 1) {
+                System.out.println(Constants.ENTER_EMPID_TO_DELETE);
+                int empIdToDelete = scanner.nextInt();
+                if (manager.deleteEmployeeById(empIdToDelete)) {
+                    System.out.println(Constants.DELETE_SUCCESS_MESSAGE + empIdToDelete);
+                } else {
+                    System.out.println(Constants.EMPLOYEE_NOT_FOUND_MESSAGE + empIdToDelete);
+                }
+            } else if (choice == 2) {
+                manager.deleteAllEmployees();
+                System.out.println(Constants.DELETE_ALL_SUCCESS_MESSAGE);
+            } else {
+                System.out.println(Constants.INVALID_CHOICE);
             }
-        } catch (InputMismatchException e) {
-            throw new EmployeeOperationException(Constants.INVALID_INPUT);
         } catch (Exception e) {
             throw new EmployeeOperationException(Constants.UNEXPECTED_ERROR + e.getMessage());
         }
