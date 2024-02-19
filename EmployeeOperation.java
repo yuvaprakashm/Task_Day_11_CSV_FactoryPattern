6241c183e934e49209d2c1a648e399356c2a1a6d
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-19 15:49:35 +0530

    Update EmployeeOperation.java

--- EmployeeOperation.java
+++ EmployeeOperation.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import net.texala.employee.exceptions.EmployeeOperationException;
 import net.texala.employee.manager.EmployeeManager;
-import net.texala.employee.model.Employee;
 
 public interface EmployeeOperation {
     void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException;
