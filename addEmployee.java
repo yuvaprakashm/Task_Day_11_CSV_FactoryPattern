1db9660a049129e391a7c2209921e00b7fe17101
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 10:39:19 +0530

    Update addEmployee.java

--- addEmployee.java
+++ addEmployee.java
@@ -1,4 +1,3 @@
-
 package net.texala.employee.impl;
 
 import java.util.Scanner;
@@ -8,25 +7,25 @@ import net.texala.employee.manager.EmployeeManager;
 import net.texala.employee.service.EmployeeOperation;
 import net.texala.employee.exceptions.EmployeeOperationException;
 
-public class addEmployee implements EmployeeOperation {
-	@Override
-	public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
-		try {
-			System.out.println(Constants.ENTER_EMPID);
-			int empId = manager.getValidEmpId(scanner);
+public class AddEmployee implements EmployeeOperation {
+    @Override
+    public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
+        try {
+            System.out.println(Constants.ENTER_EMPID);
+            int empId = manager.getValidEmpId(scanner);
 
-			System.out.println(Constants.ENTER_FIRST_NAME);
-			String firstName = scanner.nextLine();
+            System.out.println(Constants.ENTER_FIRST_NAME);
+            String firstName = scanner.nextLine();
 
-			System.out.println(Constants.ENTER_LAST_NAME);
-			String lastName = scanner.nextLine();
+            System.out.println(Constants.ENTER_LAST_NAME);
+            String lastName = scanner.nextLine();
 
-			String department = manager.selectDepartment(scanner);
+            String department = manager.selectDepartment(scanner);
 
-			manager.addEmployee(empId, firstName, lastName, department);
-		} catch (Exception e) {
-
-			EmployeeOperationException.throwAddEmployeeException(Constants.ERROR_ADD_EMPLOYEE + e.getMessage());
-		}
-	}
+            manager.addEmployee(empId, firstName, lastName, department);
+        } catch (Exception e) {
+            EmployeeOperationException.throwAddEmployeeException(Constants.ERROR_ADD_EMPLOYEE + e.getMessage());
+        }
+    }
 }
+
