ad66ce2c1ec3eae93f07ecd433c187ab9bbaf0a6
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 17:02:45 +0530

    Update addEmployee.java

--- addEmployee.java
+++ addEmployee.java
@@ -11,21 +11,28 @@ public class AddEmployee implements EmployeeOperation {
     @Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
         try {
-            System.out.println(Constants.ENTER_EMPID);
+        	System.out.println(Constants.ADD_EMPLOYEE_HEADER_TOP);
+        	System.out.println(Constants.ADD_EMPLOYEE_HEADER_MIDDLE);
+        	System.out.println(Constants.ADD_EMPLOYEE_HEADER_BOTTOM);
+
+
+            System.out.print(Constants.ENTER_EMPID);
             int empId = manager.getValidEmpId(scanner);
 
-            System.out.println(Constants.ENTER_FIRST_NAME);
+            System.out.print(Constants.ENTER_FIRST_NAME);
             String firstName = scanner.nextLine();
 
-            System.out.println(Constants.ENTER_LAST_NAME);
+            System.out.print(Constants.ENTER_LAST_NAME);
             String lastName = scanner.nextLine();
 
             String department = manager.selectDepartment(scanner);
 
+            System.out.println();
             manager.addEmployee(empId, firstName, lastName, department);
+        } catch (IllegalArgumentException e) {
+            System.out.println("\nError: " + e.getMessage());
         } catch (Exception e) {
             EmployeeOperationException.throwAddEmployeeException(Constants.ERROR_ADD_EMPLOYEE + e.getMessage());
         }
     }
 }
-
