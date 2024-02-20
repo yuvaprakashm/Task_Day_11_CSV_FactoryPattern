50fe6937e14f9fd33678276874638f3db2039683
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 10:40:39 +0530

    Update commitEmployee.java

--- commitEmployee.java
+++ commitEmployee.java
@@ -9,11 +9,16 @@ import net.texala.employee.exceptions.EmployeeOperationException;
 public class commitEmployee implements EmployeeOperation {
     @Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
-    	try {
-         
-        manager.commitChanges();
-    } catch (Exception e) {
-    	 EmployeeOperationException.throwCommitEmployeeException(Constants.ERROR_COMMIT_EMPLOYEE + e.getMessage());
-   }
-}
+        try {
+            // Display all employees before committing changes
+            System.out.println("Updated Employee Records:");
+            manager.displayAllEmployees().forEach(System.out::println);
+            
+            // Commit changes after displaying the updated records
+            manager.commitChanges();
+            System.out.println("Changes committed successfully.");
+        } catch (Exception e) {
+            EmployeeOperationException.throwCommitEmployeeException(Constants.ERROR_COMMIT_EMPLOYEE + e.getMessage());
+        }
+    }
 }
