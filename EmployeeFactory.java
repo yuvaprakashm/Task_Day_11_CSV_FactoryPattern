1688e93fd7f096c72a0e0f12b2b97acb51f4e468
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 17:01:43 +0530

    Update EmployeeFactory.java

--- EmployeeFactory.java
+++ EmployeeFactory.java
@@ -1,26 +1,26 @@
 package net.texala.employee.factory;
 
  
-import net.texala.employee.impl.addEmployee;
-import net.texala.employee.impl.commitEmployee;
-import net.texala.employee.impl.deleteEmployee;
-import net.texala.employee.impl.displayEmployee;
-import net.texala.employee.impl.updateEmployee;
+import net.texala.employee.impl.AddEmployee;
+import net.texala.employee.impl.CommitEmployee;
+import net.texala.employee.impl.DeleteEmployee;
+import net.texala.employee.impl.DisplayEmployee;
+import net.texala.employee.impl.UpdateEmployee;
 import net.texala.employee.service.EmployeeOperation;
 
 public class EmployeeFactory {
     public EmployeeOperation createEmployeeOperation(int choice) {
         switch (choice) {
             case 1:
-                return new addEmployee();
+                return new AddEmployee();
             case 2:
-                return new updateEmployee();
+                return new UpdateEmployee();
             case 3:
-                return new deleteEmployee();
+                return new DeleteEmployee();
             case 4:
-                return new displayEmployee();
+                return new DisplayEmployee();
             case 5:
-                return new commitEmployee();
+                return new CommitEmployee();
             default:
                 return null;
         }
