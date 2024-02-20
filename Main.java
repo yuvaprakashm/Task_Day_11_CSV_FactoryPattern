601d5ffaf392c2cd1d4a22b8bd95407e21864703
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 10:38:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,41 +10,50 @@ import net.texala.employee.manager.EmployeeManager;
 import net.texala.employee.service.EmployeeOperation;
 
 public class Main {
-    public static void main(String[] args) throws EmployeeOperationException {
+    public static void main(String[] args) {
         EmployeeFactory factory = new EmployeeFactory();
         Scanner scanner = new Scanner(System.in);
         EmployeeManager manager = new EmployeeManager();
-        manager.loadDataFromCSV();
-
+        
         boolean exit = false;
         while (!exit) {
-            System.out.println(Constants.MENU_OPTION_ADD);
-            System.out.println(Constants.MENU_OPTION_UPDATE);
-            System.out.println(Constants.MENU_OPTION_DELETE);
-            System.out.println(Constants.MENU_OPTION_DISPLAY);
-            System.out.println(Constants.MENU_OPTION_COMMIT);
-            System.out.println(Constants.MENU_OPTION_EXIT);
-            System.out.print(Constants.MENU_OPTION_ENTER_CHOICE);
-
+            // Display menu options
+        	System.out.println("============================================");
+        	System.out.println("|         Employee Management System        |");
+        	System.out.println("============================================");
+        	System.out.println("| " + Constants.MENU_OPTION_ADD);
+        	System.out.println("| " + Constants.MENU_OPTION_UPDATE);
+        	System.out.println("| " + Constants.MENU_OPTION_DELETE);
+        	System.out.println("| " + Constants.MENU_OPTION_DISPLAY);
+        	System.out.println("| " + Constants.MENU_OPTION_COMMIT);
+        	System.out.println("| " + Constants.MENU_OPTION_EXIT);
+        	System.out.println("============================================");
+        	System.out.print(Constants.MENU_OPTION_ENTER_CHOICE);
             try {
                 int choice = scanner.nextInt();
-                scanner.nextLine();
+                scanner.nextLine(); // Consume newline character
 
-                if (choice == Constants.EXIT) {
-                    exit = true;
-                    continue;
-                }
-
-                EmployeeOperation operation = factory.createEmployeeOperation(choice);
-                if (operation != null) {
-                    operation.execute(manager, scanner);
-                } else {
-                	System.out.println(Constants.ERROR_INVALID_CHOICE);
+                switch (choice) {
+                    case Constants.EXIT:
+                    	manager.commitAndExit();
+                       // System.out.println(Constants.MESSAGE_PROGRAM_TERMINATED);// Commit changes before exiting
+                        exit = true;
+                        break;
+                    default:
+                        EmployeeOperation operation = factory.createEmployeeOperation(choice);
+                        if (operation != null) {
+                            operation.execute(manager, scanner);
+                        } else {
+                            System.out.println(Constants.ERROR_INVALID_CHOICE);
+                        }
+                        break;
                 }
             } catch (InputMismatchException e) {
-            	 System.out.println(Constants.ERROR_INVALID_INPUT);
-                scanner.nextLine();
-            }  
+                System.out.println(Constants.ERROR_INVALID_INPUT);
+                scanner.nextLine(); // Consume newline character
+            } catch (EmployeeOperationException e) {
+                System.out.println(e.getMessage());
+            }
         }
         scanner.close();
     }
