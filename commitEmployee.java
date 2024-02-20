ff6d6e21ffcf854b3ec23483b7ff566a074d9710
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 17:02:58 +0530

    Update commitEmployee.java

--- commitEmployee.java
+++ commitEmployee.java
@@ -6,17 +6,20 @@ import net.texala.employee.service.EmployeeOperation;
 import net.texala.employee.constants.Constants;
 import net.texala.employee.exceptions.EmployeeOperationException;
 
-public class commitEmployee implements EmployeeOperation {
+public class CommitEmployee implements EmployeeOperation {
+
     @Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
         try {
-            // Display all employees before committing changes
-            System.out.println("Updated Employee Records:");
-            manager.displayAllEmployees().forEach(System.out::println);
-            
-            // Commit changes after displaying the updated records
             manager.commitChanges();
-            System.out.println("Changes committed successfully.");
+            System.out.println(Constants.ADD_EMPLOYEE_HEADER_TOP);
+            System.out.println(Constants.COMMIT_EMPLOYEE_HEADER_MIDDLE);
+            System.out.println(Constants.ADD_EMPLOYEE_HEADER_BOTTOM);
+
+            System.out.println(Constants.UP_EMP);
+            System.out.println(Constants.SORTED_EMPLOYEE_RECORDS_HEADER);
+            
+            manager.displayAllEmployees().forEach(System.out::println);
         } catch (Exception e) {
             EmployeeOperationException.throwCommitEmployeeException(Constants.ERROR_COMMIT_EMPLOYEE + e.getMessage());
         }
