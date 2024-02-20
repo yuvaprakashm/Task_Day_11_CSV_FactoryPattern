1c4f625fdc438ac5974abf8987fc7502b882bbd8
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-20 17:04:55 +0530

    Update updateEmployee.java

--- updateEmployee.java
+++ updateEmployee.java
@@ -7,106 +7,75 @@ import net.texala.employee.manager.EmployeeManager;
 import net.texala.employee.model.Employee;
 import net.texala.employee.service.EmployeeOperation;
 
-public class updateEmployee implements EmployeeOperation {
+public class UpdateEmployee implements EmployeeOperation {
 
     @Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
         try {
-            while (true) {
+            boolean validEmployeeId = false;
+            int empIdToUpdate = 0;
+
+            while (!validEmployeeId) {
                 System.out.println(Constants.ENTER_EMPID_TO_UPDATE);
-                int empIdToUpdate;
                 try {
                     empIdToUpdate = Integer.parseInt(scanner.nextLine());
+                    validEmployeeId = true;
                 } catch (NumberFormatException e) {
                     System.out.println(Constants.INVALID_EMPLOYEE_ID);
-                    continue;
                 }
+            }
 
-                Employee employeeToUpdate = manager.getEmployeeByEmpId(empIdToUpdate);
-                if (employeeToUpdate == null) {
-                    System.out.println(String.format(Constants.EMPLOYEE_NOT_FOUND, empIdToUpdate));
-                    continue;
-                }
+            Employee employeeToUpdate = manager.getEmployeeByEmpId(empIdToUpdate);
+            if (employeeToUpdate == null) {
+                System.out.println(String.format(Constants.EMPLOYEE_NOT_FOUND, empIdToUpdate));
+                return;
+            }
 
-                System.out.println(Constants.CHOOSE_FIELD_TO_UPDATE);
-                System.out.println(Constants.UPDATE_OPTION_FIRST_NAME);
-                System.out.println(Constants.UPDATE_OPTION_LAST_NAME);
-                System.out.println(Constants.UPDATE_OPTION_DEPARTMENT);
-                System.out.println(Constants.UPDATE_OPTION_ALL);
-                System.out.print(Constants.SELECT_FIELD);
-                int fieldChoice = scanner.nextInt();
-                scanner.nextLine();
-                String fieldToUpdate;
-                switch (fieldChoice) {
-                    case Constants.FIELD_FIRST_NAME:
-                        fieldToUpdate = Constants.FIELD_FIRST_NAME_STRING;
-                        break;
-                    case Constants.FIELD_LAST_NAME:
-                        fieldToUpdate = Constants.FIELD_LAST_NAME_STRING;
-                        break;
-                    case Constants.FIELD_DEPARTMENT:
-                        fieldToUpdate = Constants.FIELD_DEPARTMENT_STRING;
-                        break;
-                    case Constants.UPDATE__ALL:
-                        fieldToUpdate = Constants.UPDATE_ALL_STRING;
-                        break;
-                    default:
-                        System.out.println(Constants.INVALID_FIELD_CHOICE);
-                        return;
-                }
-                if (!fieldToUpdate.equals(Constants.UPDATE_ALL_STRING)) {
-                    if (!fieldToUpdate.equals(Constants.FIELD_DEPARTMENT_STRING)) {
-                        System.out.println(Constants.ENTER_NEW_VALUE);
-                        String newValue = scanner.nextLine();
-                        manager.updateEmployee(empIdToUpdate, fieldToUpdate, newValue);
-                    } else {
-                        System.out.println(Constants.ENTER_NEW_DEPARTMENT);
-                        System.out.println(Constants.DEV_OPTION);
-                        System.out.println(Constants.PS_OPTION);
-                        System.out.println(Constants.QA_OPTION);
-                        System.out.println(Constants.ADMIN_OPTION);
-                        System.out.print(Constants.SELECT_DEPARTMENT);
-                        int departmentChoice = scanner.nextInt();
-                        scanner.nextLine();
-                        String newDepartment;
-                        switch (departmentChoice) {
-                            case Constants.DEV_OPTION_CHOICE:
-                                newDepartment = Constants.DEV_STRING;
-                                break;
-                            case Constants.PS_OPTION_CHOICE:
-                                newDepartment = Constants.PS_STRING;
-                                break;
-                            case Constants.QA_OPTION_CHOICE:
-                                newDepartment = Constants.QA_STRING;
-                                break;
-                            case Constants.ADMIN_OPTION_CHOICE:
-                                newDepartment = Constants.ADMIN_STRING;
-                                break;
-                            default:
-                                System.out.println(Constants.INVALID_DEPARTMENT_CHOICE);
-                                newDepartment = Constants.UNKNOWN_STRING;
-                                break;
-                        }
+            System.out.println(Constants.EMPLOYEE_HEADER);
+            System.out.println(Constants.UPDATE_OPTION_FIRST_NAME);
+            System.out.println(Constants.UPDATE_OPTION_LAST_NAME);
+            System.out.println(Constants.UPDATE_OPTION_DEPARTMENT);
+            System.out.println(Constants.UPDATE_OPTION_ALL);
+            System.out.print(Constants.SELECT_FIELD + " ");
 
-                        manager.updateEmployee(empIdToUpdate, fieldToUpdate, newDepartment);
-                    }
+            int fieldChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            String fieldToUpdate;
+            switch (fieldChoice) {
+                case Constants.FIELD_FIRST_NAME:
+                    fieldToUpdate = Constants.FIELD_FIRST_NAME_STRING;
+                    break;
+                case Constants.FIELD_LAST_NAME:
+                    fieldToUpdate = Constants.FIELD_LAST_NAME_STRING;
+                    break;
+                case Constants.FIELD_DEPARTMENT:
+                    fieldToUpdate = Constants.FIELD_DEPARTMENT_STRING;
+                    break;
+                case Constants.UPDATE__ALL:
+                    fieldToUpdate = Constants.UPDATE_ALL_STRING;
+                    break;
+                default:
+                    System.out.println(Constants.INVALID_FIELD_CHOICE);
+                    return;
+            }
+
+            if (!fieldToUpdate.equals(Constants.UPDATE_ALL_STRING)) {
+                if (!fieldToUpdate.equals(Constants.FIELD_DEPARTMENT_STRING)) {
+                    System.out.println(Constants.ENTER_NEW_VALUE);
+                    String newValue = scanner.nextLine();
+                    manager.updateEmployee(empIdToUpdate, fieldToUpdate, newValue);
                 } else {
-                    System.out.println(Constants.ENTER_NEW_VALUES);
-                    System.out.println(Constants.ENTER_NEW_EMPID);
-                    int newEmpId = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println(Constants.ENTER_NEW_FIRST_NAME);
-                    String newFirstName = scanner.nextLine();
-                    System.out.println(Constants.ENTER_NEW_LAST_NAME);
-                    String newLastName = scanner.nextLine();
                     System.out.println(Constants.ENTER_NEW_DEPARTMENT);
                     System.out.println(Constants.DEV_OPTION);
                     System.out.println(Constants.PS_OPTION);
                     System.out.println(Constants.QA_OPTION);
                     System.out.println(Constants.ADMIN_OPTION);
-                    System.out.print(Constants.SELECT_DEPARTMENT);
+                    System.out.print(Constants.SELECT_DEPARTMENT + " ");
+
                     int departmentChoice = scanner.nextInt();
                     scanner.nextLine();
+
                     String newDepartment;
                     switch (departmentChoice) {
                         case Constants.DEV_OPTION_CHOICE:
@@ -126,12 +95,53 @@ public class updateEmployee implements EmployeeOperation {
                             newDepartment = Constants.UNKNOWN_STRING;
                             break;
                     }
-                    manager.updateEmployee(empIdToUpdate, fieldToUpdate, newEmpId, newFirstName, newLastName, newDepartment);
+
+                    manager.updateEmployee(empIdToUpdate, fieldToUpdate, newDepartment);
                 }
+            } else {
+                System.out.println(Constants.ENTER_NEW_VALUES);
+                System.out.println(Constants.ENTER_NEW_EMPID + " ");
+                int newEmpId = scanner.nextInt();
+                scanner.nextLine();
                 
-                 
-                break;
+                System.out.println(Constants.ENTER_NEW_FIRST_NAME + " ");
+                String newFirstName = scanner.nextLine();
+                
+                System.out.println(Constants.ENTER_NEW_LAST_NAME + " ");
+                String newLastName = scanner.nextLine();
+                
+                System.out.println(Constants.ENTER_NEW_DEPARTMENT);
+                System.out.println(Constants.DEV_OPTION);
+                System.out.println(Constants.PS_OPTION);
+                System.out.println(Constants.QA_OPTION);
+                System.out.println(Constants.ADMIN_OPTION);
+                System.out.print(Constants.SELECT_DEPARTMENT + " ");
+                
+                int departmentChoice = scanner.nextInt();
+                scanner.nextLine();
+                
+                String newDepartment;
+                switch (departmentChoice) {
+                    case Constants.DEV_OPTION_CHOICE:
+                        newDepartment = Constants.DEV_STRING;
+                        break;
+                    case Constants.PS_OPTION_CHOICE:
+                        newDepartment = Constants.PS_STRING;
+                        break;
+                    case Constants.QA_OPTION_CHOICE:
+                        newDepartment = Constants.QA_STRING;
+                        break;
+                    case Constants.ADMIN_OPTION_CHOICE:
+                        newDepartment = Constants.ADMIN_STRING;
+                        break;
+                    default:
+                        System.out.println(Constants.INVALID_DEPARTMENT_CHOICE);
+                        newDepartment = Constants.UNKNOWN_STRING;
+                        break;
+                }
+                manager.updateEmployee(empIdToUpdate, fieldToUpdate, newEmpId, newFirstName, newLastName, newDepartment);
             }
+        
         } catch (Exception e) {
             throw new EmployeeOperationException(Constants.UPDATE_EMPLOYEE_ERROR_MESSAGE + e.getMessage());
         }
