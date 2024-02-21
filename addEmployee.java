e207043fb7ca3d5d10d338c242ce768251f6f3c4
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-21 16:57:16 +0530

    Update addEmployee.java

--- addEmployee.java
+++ addEmployee.java
@@ -2,27 +2,34 @@ package net.texala.employee.impl;
 
 import java.util.Scanner;
 
-import net.texala.employee.constants.Constants;
 import net.texala.employee.manager.EmployeeManager;
 import net.texala.employee.service.EmployeeOperation;
 import net.texala.employee.exceptions.EmployeeOperationException;
+import static net.texala.employee.constants.Constants.ADD_EMPLOYEE_HEADER_TOP;
+import static net.texala.employee.constants.Constants.ADD_EMPLOYEE_HEADER_MIDDLE;
+import static net.texala.employee.constants.Constants.ADD_EMPLOYEE_HEADER_BOTTOM;
+import static net.texala.employee.constants.Constants.ENTER_EMPID;
+import static net.texala.employee.constants.Constants.ENTER_FIRST_NAME;
+import static net.texala.employee.constants.Constants.ENTER_LAST_NAME;
+import static net.texala.employee.constants.Constants.ERROR_ADD_EMPLOYEE;
+
 
 public class AddEmployee implements EmployeeOperation {
     @Override
     public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
         try {
-        	System.out.println(Constants.ADD_EMPLOYEE_HEADER_TOP);
-        	System.out.println(Constants.ADD_EMPLOYEE_HEADER_MIDDLE);
-        	System.out.println(Constants.ADD_EMPLOYEE_HEADER_BOTTOM);
+        	System.out.println(ADD_EMPLOYEE_HEADER_TOP);
+        	System.out.println(ADD_EMPLOYEE_HEADER_MIDDLE);
+        	System.out.println(ADD_EMPLOYEE_HEADER_BOTTOM);
 
 
-            System.out.print(Constants.ENTER_EMPID);
+            System.out.print(ENTER_EMPID);
             int empId = manager.getValidEmpId(scanner);
 
-            System.out.print(Constants.ENTER_FIRST_NAME);
+            System.out.print(ENTER_FIRST_NAME);
             String firstName = scanner.nextLine();
 
-            System.out.print(Constants.ENTER_LAST_NAME);
+            System.out.print(ENTER_LAST_NAME);
             String lastName = scanner.nextLine();
 
             String department = manager.selectDepartment(scanner);
@@ -32,7 +39,7 @@ public class AddEmployee implements EmployeeOperation {
         } catch (IllegalArgumentException e) {
             System.out.println("\nError: " + e.getMessage());
         } catch (Exception e) {
-            EmployeeOperationException.throwAddEmployeeException(Constants.ERROR_ADD_EMPLOYEE + e.getMessage());
+            EmployeeOperationException.throwAddEmployeeException(ERROR_ADD_EMPLOYEE + e.getMessage());
         }
     }
 }
